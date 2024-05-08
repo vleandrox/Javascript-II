29989684d629117635d41650b6213e1178574cb4
vleandrox <victor.alarcon.t@hotmail.com>
2024-05-08 18:20:21 -0500

    Asignación con AND lógico

--- Profile/profile.js
+++ Profile/profile.js
@@ -5,10 +5,21 @@ const ubicacion = "Huacho"
 const hobby = "Videojuegos"
 
 const datosSelector = document.querySelector("#datospersonales");
-console.log(datosSelector)
+// console.log(datosSelector)
 
-const templateDatos = `
-<span>Mi nombre es <b>${nombre}</b> y mi Apellido es <b>${apellido}</b>, tengo la profesion de <b>${profesion}</b>.</span>
-<br><span>Radico en la ciudad de <b>${ubicacion}</b> - Lima y en mis tiempos libres me gusta jugar <b>${hobby}</b></span>`;
+// Implementando la Asignación con AND lógico (&&)
+let isOnline = false
+const pregunta = prompt("¿ESTAS CONECTADO? SI o NO")
 
-datosSelector.innerHTML = templateDatos
+if(pregunta.toUpperCase() == 'SI'){
+    isOnline = true
+    console.log(isOnline)
+    const templateDatos = `
+    <span>Mi nombre es <b>${nombre}</b> y mi Apellido es <b>${apellido}</b>, tengo la profesion de <b>${profesion}</b>.</span>
+    <br><span>Radico en la ciudad de <b>${ubicacion}</b> - Lima y en mis tiempos libres me gusta jugar <b>${hobby}</b></span>`;
+    datosSelector.innerHTML = templateDatos
+}else{
+    isOnline = false
+    console.log(isOnline)
+    window.alert("USUARIO ESTA DESCONECTADO !!!")
+}
